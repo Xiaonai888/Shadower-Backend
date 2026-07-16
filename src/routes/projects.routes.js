31dfed1e82d013b7c28fc908f58206e96d7eab1e
Xiaonai <xiaonai.ns1@gmail.com>
2026-07-16 21:33:07 +0700

    Update projects.routes.js

--- src/routes/projects.routes.js
+++ src/routes/projects.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import {
   getProject,
+  getProjectSchema,
   getProjects
 } from "../controllers/projects.controller.js";
 
 const router = Router();
 
 router.get("/", getProjects);
+router.get("/schema", getProjectSchema);
 router.get("/:id", getProject);
 
 export default router;
