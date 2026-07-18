c118e11f499ea406b708ec31f2a3837769baadd3
Xiaonai <xiaonai.ns1@gmail.com>
2026-07-18 09:54:38 +0700

    Update voiceCharacters.routes.js

--- src/routes/voiceCharacters.routes.js
+++ src/routes/voiceCharacters.routes.js
@@ -6,12 +6,20 @@ import {
   getVoiceCharacters,
   updateVoiceCharacter
 } from "../controllers/voiceCharacters.controller.js";
+import {
+  completeVoiceAvatarUpload,
+  deleteVoiceAvatar,
+  requestVoiceAvatarUpload
+} from "../controllers/voiceAvatar.controller.js";
 import voiceSamplesRoutes from "./voiceSamples.routes.js";
 
 const router = Router();
 
 router.get("/", getVoiceCharacters);
 router.post("/", createVoiceCharacter);
+router.post("/:id/avatar/upload-url", requestVoiceAvatarUpload);
+router.post("/:id/avatar/complete", completeVoiceAvatarUpload);
+router.delete("/:id/avatar", deleteVoiceAvatar);
 router.use("/:characterId/samples", voiceSamplesRoutes);
 router.get("/:id", getVoiceCharacter);
 router.patch("/:id", updateVoiceCharacter);
